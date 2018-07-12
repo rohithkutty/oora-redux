de660fd63561590152579cc3b3660bd38310325c
Rohith Surya <rohith_gonela@yahoo.com>
2018-07-12 12:37:01 +0530

    added post form to the application - complete redux app

--- src/Components/Postform.js
+++ src/Components/Postform.js
@@ -1,4 +1,7 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { createPost } from '../actions/postAction';
 
 class PostForm extends Component {
   constructor(props) {
@@ -25,15 +28,7 @@ class PostForm extends Component {
       body: this.state.body
     }
 
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json'
-      },
-      body: JSON.stringify(post)
-    })
-    .then(res => res.json())
-    .then(data => console.log(data))
+    this.props.createPost(post);
   }
 
   render() {
@@ -58,4 +53,10 @@ class PostForm extends Component {
   }
 }
 
-export default PostForm;
+PostForm.prototypes = {
+  title: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired,
+  createPost: PropTypes.func.isRequired
+}
+
+export default connect(null, {createPost})(PostForm);
